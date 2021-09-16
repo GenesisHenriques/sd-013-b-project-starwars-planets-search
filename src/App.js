cb35ada0c42059c12f249e251008d21d289dba9b
eaeniko <eaenikolai@gmail.com>
2021-09-16 14:36:59 -0300

    creaete Home files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+// import { Switch } from '.react-router-dom';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Home />
   );
 }
 
