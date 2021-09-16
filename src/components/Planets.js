b5d982f916f38a8fd116c389f1cae1da1d89557c
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-09-16 16:06:08 -0300

    Challenges 5 and 6 completed

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -6,44 +6,125 @@ import FilterPlanetsByNumber from './FilterPlanetsByNumber';
 function Planets() {
   const [data, setData] = useState([]);
   const { filters, setFilters } = useProvider();
-
+  const { filterByNumericValues, order } = filters;
+  const minusOne = -1;
+  
   useEffect(() => {
     fetch('https://swapi-trybe.herokuapp.com/api/planets/')
       .then((response) => response.json())
       .then((json) => setData(json.results));
   }, []);
 
+  function orderString(word) {
+    if (order.sort === 'ASC') {
+      return word.sort((a, b) => {
+        if (a.name < b.name) {
+          return minusOne;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    return word.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return minusOne;
+      }
+      return 0;
+    });
+  }
+
+  function orderNumber(number) {
+    console.log(number);
+    if (order.sort === 'ASC') {
+      return number.sort((a, b) => (
+        a.orbital_period.localeCompare(b.orbital_period, undefined, {
+          numeric: true,
+          sensitivity: 'base',
+        })
+      ));
+    }
+    return number.sort((a, b) => b.orbital_period - a.orbital_period);
+  }
+
+  function orderData(dataList) {
+    if (order.column === 'Name') {
+      const dataOrder = orderString(dataList);
+
+      return dataOrder;
+    }
+    const dataOrder = orderNumber(dataList);
+
+    return dataOrder;
+  }
+
+  const [localSort, setLocalSort] = useState({
+    order: {
+      column: 'Name',
+      sort: 'ASC',
+    },
+  });
+
+  function orderList() {
+    console.log({
+      ...filters,
+      ...localSort,
+    });
+    setFilters({
+      ...filters,
+      ...localSort,
+    });
+  }
+
   function filterData(rows) {
     const { filterByName: { name } } = filters;
-    const { filterByNumericValues } = filters;
+    const results = [];
     const minusOne = -1;
 
     if (filterByNumericValues.length > 0) {
-      const { column, comparison, value } = filterByNumericValues[0];
-      switch (comparison) {
-      case 'maior que':
-        return rows.filter((row) => (
-          row.name.toLowerCase().indexOf(name.toLowerCase() > minusOne)
-          && parseInt(row[column], 0) > parseInt(value, 0)
+      filterByNumericValues.forEach((filter) => {
+        const { column, comparison, value } = filter;
+        switch (comparison) {
+          case 'maior que':
+            results.push(rows.filter((row) => (
+              row.name.toLowerCase().indexOf(name.toLowerCase()) > minusOne && parseInt(row[column], 0) > parseInt(value, 0))));
+            break;
+          case 'menor que':
+            results.push(rows.filter((row) => (
+              row.name.toLowerCase().indexOf(name.toLowerCase()) > minusOne && parseInt(row[column], 0) < parseInt(value, 0))));
+            break;
+          case 'igual a':
+            results.push(rows.filter((row) => (
+              row.name.toLowerCase().indexOf(name.toLowerCase()) > minusOne && parseInt(row[column], 0) === parseInt(value, 0))));
+            break;
+          default:
+        }
+      });
+      if (results[0] && results[1]) {
+        const finalFilter = results[0].filter((planet) => (
+          results[1].includes(planet)
         ));
-      case 'menor que':
-        return rows.filter((row) => (
-          row.name.toLowerCase().indexOf(name.toLowerCase() > minusOne)
-          && parseInt(row[column], 0) < parseInt(value, 0)
-        ));
-      case 'igual a':
-        return rows.filter((row) => (
-          row.name.toLowerCase().indexOf(name.toLowerCase() > minusOne)
-          && parseInt(row[column], 0) === parseInt(value, 0)
-        ));
-      default:
+        return finalFilter;
       }
+      return results[0];
     }
-
     return rows.filter((row) => (
       row.name.toLowerCase().indexOf(name.toLowerCase()) > minusOne
     ));
   }
+
+  const deleteFilter = () => {
+    const checkFilters = filterByNumericValues;
+    checkFilters.shift();
+    setFilters({
+      ...filters,
+      filterByNumericValues: checkFilters,
+    });
+  };
 
   return (
     <div>
@@ -54,6 +135,7 @@ function Planets() {
           id="name-filter"
           data-testid="name-filter"
           onChange={ (e) => setFilters({
+            ...filters,
             filterByName: {
               name: e.target.value,
             },
@@ -61,7 +143,69 @@ function Planets() {
         />
       </label>
       <FilterPlanetsByNumber />
-      <PlanetsTable dataTable={ filterData(data) } />
+      { filterByNumericValues.length > 0
+      && (
+        filterByNumericValues.map((value, index) => (
+          <div data-testid="filter" key={ index }>
+            <span>{ `${value.column} ` }</span>
+            <span>{ `${value.comparison} ` }</span>
+            <span>{ `${value.value} ` }</span>
+            <button
+              onClick={ deleteFilter }
+              type="button"
+            >
+              X
+            </button>
+          </div>))
+      )}
+      <select
+        data-testid="column-sort"
+        onChange={ (e) => setLocalSort({
+          order: {
+            ...localSort.order,
+            column: e.target.value,
+          },
+        }) }
+      >
+        <option value="Name">Name</option>
+        <option value="orbital_period">Orbital Period</option>
+      </select>
+
+      <div
+        onChange={ (e) => setLocalSort({
+          order: {
+            ...localSort.order,
+            sort: e.target.id,
+          },
+        }) }
+      >
+        <label htmlFor="ASC">
+          Crescente
+          <input
+            type="radio"
+            name="sort"
+            id="ASC"
+            data-testid="column-sort-inpot-asc"
+          />
+        </label>
+        <label htmlFor="DESC">
+          Decrescente
+          <input
+            type="radio"
+            name="sort"
+            id="DESC"
+            data-testid="column-sort-input-desc"
+          />
+        </label>
+      </div>
+      <button
+        data-testid="column-sort-button"
+        type="button"
+        onClick={ orderList }
+      >
+        Ordenar
+      </button>
+      <PlanetsTable dataTable={ orderData(filterData(data)) } />
     </div>
   );
 }
