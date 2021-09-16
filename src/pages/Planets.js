a480273ff71bd223407a5974c06cfdcf21ba5213
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-16 13:54:55 -0300

    Requisito 2

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ResidentTable from '../components/ResidentTable';
+import NameFilter from '../components/NameFilter';
 
 function Planets() {
   return (
     <div>
+      <NameFilter />
       <ResidentTable />
     </div>
   );
