7d64ed2131dfca2d98c3c314b9dcc08031034793
Leonardo Martins <leonardomartins07@gmail.com>
2021-09-16 18:18:31 -0300

    requisito 1

---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import PlanetsContext from './PlanetsContext';
+
+function PlanetsProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.results);
+      });
+  }, []);
+
+  return (
+    <PlanetsContext.Provider
+      value={ { data } }
+    >
+      {children}
+    </PlanetsContext.Provider>
+  );
+}
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default PlanetsProvider;
