68f51b6d14d88bdb8f868a5725f3c18e5c4ef44b
pro gui <ggiacomini2012@gmail.com>
2021-09-16 15:06:30 -0300

    setting up context, pages, components, services and hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
+import Context from './context/Context';
 
 function App() {
+  const global = useContext(Context);
+  console.log(global);
+
   return (
+
     <span>Hello, App !!!</span>
+
   );
 }
 
