3764a0b32b5b13a34f4261d6b96a86364e0c1da7
Leonardo Martins <leonardomartins07@gmail.com>
2021-09-16 19:17:27 -0300

    requisito 2

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,45 +2,61 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data } = useContext(PlanetsContext);
+  const { contextValue } = useContext(PlanetsContext);
+  const { filters, filters: { filterByName: { name } }, setFilters } = contextValue;
+  let { data } = contextValue;
+
+  if (name) data = data.filter((planet) => planet.name.toLowerCase().includes(name));
+
+  function handleFilter({ target: { value } }) {
+    setFilters({
+      ...filters,
+      filterByName: {
+        name: value,
+      },
+    });
+  }
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>name</th>
-          <th>rotation_period</th>
-          <th>orbital_period</th>
-          <th>diameter</th>
-          <th>climate</th>
-          <th>gravity</th>
-          <th>terrain</th>
-          <th>surface_water</th>
-          <th>population</th>
-          <th>films</th>
-          <th>created</th>
-          <th>edited</th>
-          <th>url</th>
-        </tr>
-        {data.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+    <div>
+      <input type="text" data-testid="name-filter" onChange={ handleFilter } />
+      <table>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <th>rotation_period</th>
+            <th>orbital_period</th>
+            <th>diameter</th>
+            <th>climate</th>
+            <th>gravity</th>
+            <th>terrain</th>
+            <th>surface_water</th>
+            <th>population</th>
+            <th>films</th>
+            <th>created</th>
+            <th>edited</th>
+            <th>url</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+          {data.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -6,17 +6,27 @@ function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-    // fetch('https://swapi.dev/api/planets')
+    // fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+    fetch('https://swapi.dev/api/planets')
       .then((response) => response.json())
       .then((response) => {
         setData(response.results);
       });
   }, []);
 
+  const [filters, setFilters] = useState({
+    filterByName: { name: '' },
+  });
+
+  const contextValue = {
+    data,
+    filters,
+    setFilters,
+  };
+
   return (
     <PlanetsContext.Provider
-      value={ { data } }
+      value={ { contextValue } }
     >
       {children}
     </PlanetsContext.Provider>
