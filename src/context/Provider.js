778e78300c0114185f0c4e7abaa9288732360235
Julio Pereira <jandrade.pereira1@gmail.com>
2021-09-16 17:52:54 -0300

    Requisito 1 - Adiciona os arqvuios context e provider e pasta e arquivos do componente Table

---
+++ src/context/Provider.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+export default function Provider({ children }) {
+  const [data, setData] = useState([]);
+
+  async function getPlanets() {
+    try {
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const { results } = await fetch(endpoint)
+        .then((response) => response.json());
+      setData(results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPlanets();
+  }, []);
+
+  const contextValue = {
+    data,
+    getPlanets,
+  };
+
+  return (
+    <Context.Provider value={ contextValue }>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
