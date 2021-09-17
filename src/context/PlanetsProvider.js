bb34ad3a89027650a2c6c212be5babe95ea0c21f
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-09-17 11:36:54 -0300

    :art: refactoring elements and naming  variables
    deleting codes and improving code readability

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import PlanetsContext from './PlanetsContext';
 
-function Provider({ children }) {
+function PlanetsProvider({ children }) {
   const [data, setReturnOfApi] = useState([]);
   const [filters, setFilters] = useState({
     filterByName: {
@@ -16,12 +16,11 @@ function Provider({ children }) {
       },
     ],
   });
-  const [arrayFiltered, setArrayFiltered] = useState([]);
   const [input, setInput] = useState('');
   const [selectCollum, setSelectCollum] = useState('population');
   const [selectNumber, setSelectNumber] = useState('');
   const [inputNumber, setNumber] = useState(0);
-  const [switchh, setSwitch] = useState(false);
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   useEffect(() => {
     const newObj = {
@@ -45,50 +44,25 @@ function Provider({ children }) {
     return false;
   };
 
-  const handleClickSearch = (planets, select, maq, value) => {
-    setSwitch((prevState) => !prevState);
-    switch (maq) {
-    case 'maior que':
-      setArrayFiltered(planets.filter((element) => element[select] > Number(value)));
-      break;
-    case 'menor que':
-      setArrayFiltered(planets.filter((element) => element[select] <= value));
-      break;
-    case 'igual a':
-      setArrayFiltered(planets.filter((element) => element[select] === value));
-      break;
-    default:
-      setArrayFiltered(planets);
-      break;
-    }
-  };
-
-  //   else{
-  //     return
-  //   }
-  //   // switch (choiceSearch) {
-  //   // case 'menor que':
-  //   //   console.log(choiceSearch);
-  //   //   return planetss.filter((element) => element.population === pop)
-  //   //   .filter((element) => element.pop < );
-  //   // default:
-  //   //   return planetss;
-  //   // }
-  // };
-
-  // useEffect(() => {
-  //   if (input !== '') {
-  //     setFilters((prevState) => ({
-  //       ...prevState.filters,
-  //       filterByName: { name: input },
-  //     }));
-  //   }
-  // }, [input]);
-
-  const filterByName = (planets) => {
+  const filteredArray = (planets, select, maq, value) => {
     const { filterByName: { name } } = filters;
-    const filterName = planets.filter((element) => element.name.includes(name));
-    return filterName;
+    if (name !== '') {
+      const filterName = planets.filter((element) => element.name.includes(name));
+      return filterName;
+    }
+    if (toggleSearch) {
+      switch (maq) {
+      case 'maior que':
+        return planets.filter((element) => element[select] > Number(value));
+      case 'menor que':
+        return planets.filter((element) => element[select] <= value);
+      case 'igual a':
+        return planets.filter((element) => element[select] === value);
+      default:
+        return planets;
+      }
+    }
+    return data;
   };
 
   const fetchApi = async () => {
@@ -104,18 +78,16 @@ function Provider({ children }) {
 
   const context = {
     data,
-    setInput,
-    filterByName,
-    setSelectCollum,
-    setSelectNumber,
-    setNumber,
-    handleClickSearch,
     selectCollum,
     selectNumber,
     inputNumber,
-    arrayFiltered,
-    switchh,
+    setInput,
+    setSelectCollum,
+    setSelectNumber,
+    setNumber,
     verifyLength,
+    setToggleSearch,
+    filteredArray,
   };
 
   return (
@@ -125,8 +97,8 @@ function Provider({ children }) {
   );
 }
 
-Provider.propTypes = {
-  children: PropTypes.string.isRequired,
+PlanetsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
-export default Provider;
+export default PlanetsProvider;
--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -2,22 +2,25 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 import Input from '../components/Input';
 import './Table.css';
-import Filtered from '../components/Filter';
 
 function Table() {
-  const { data, filterByName, handleClickSearch, selectCollum,
-    selectNumber, inputNumber, switchh } = useContext(PlanetsContext);
+  const
+    { data,
+      selectCollum,
+      selectNumber,
+      inputNumber,
+      setToggleSearch,
+      filteredArray,
+    } = useContext(PlanetsContext);
   if (data === undefined) {
     return <p>Loading...</p>;
   }
-  const filteredByName = filterByName(data);
-
   return (
     <fieldset>
       <Input />
       <button
         data-testid="button-filter"
-        onClick={ () => handleClickSearch(data, selectCollum, selectNumber, inputNumber) }
+        onClick={ () => setToggleSearch((prevState) => !prevState) }
         type="button"
       >
         Button
@@ -33,23 +36,24 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {switchh === false ? filteredByName.map((element) => (
-            <tr key={ data.name }>
-              <td className="tr">{element.name}</td>
-              <td className="tr">{element.rotation_period}</td>
-              <td className="tr">{element.orbital_period}</td>
-              <td className="tr">{element.diameter}</td>
-              <td className="tr">{element.climate}</td>
-              <td className="tr">{element.gravity}</td>
-              <td className="tr">{element.terrain}</td>
-              <td className="tr">{element.surface_water}</td>
-              <td className="tr">{element.population}</td>
-              <td className="tr">{element.films}</td>
-              <td className="tr">{element.created}</td>
-              <td className="tr">{element.edited}</td>
-              <td className="tr">{element.url}</td>
-            </tr>
-          )) : <Filtered />}
+          {filteredArray(data, selectCollum, selectNumber, inputNumber)
+            .map((element, index) => (
+              <tr key={ index }>
+                <td className="tr">{element.name}</td>
+                <td className="tr">{element.rotation_period}</td>
+                <td className="tr">{element.orbital_period}</td>
+                <td className="tr">{element.diameter}</td>
+                <td className="tr">{element.climate}</td>
+                <td className="tr">{element.gravity}</td>
+                <td className="tr">{element.terrain}</td>
+                <td className="tr">{element.surface_water}</td>
+                <td className="tr">{element.population}</td>
+                <td className="tr">{element.films}</td>
+                <td className="tr">{element.created}</td>
+                <td className="tr">{element.edited}</td>
+                <td className="tr">{element.url}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </fieldset>
