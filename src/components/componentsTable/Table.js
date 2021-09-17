cffaa4668c283e27e8a65ea18774fd10ecad948e
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-16 21:19:42 -0300

    configura pastas com componentes

---
+++ src/components/componentsTable/Table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
+
+function Table() {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody />
+    </table>
+  );
+}
+export default Table;
---
+++ src/components/componentsTable/TableBody.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import MyContext from '../../context/MyContext';
+
+function TableBody() {
+  const { planetsData, planetsFiltered } = useContext(MyContext);
+  return (
+    <tbody>
+      { planetsData.length > 0
+       && planetsFiltered.map((planet) => (
+         <tr key={ planet.name }>
+           <td data-testid="planet-name">{planet.name}</td>
+           <td>{planet.rotation_period}</td>
+           <td>{planet.orbital_period}</td>
+           <td>{planet.diameter}</td>
+           <td>{planet.climate}</td>
+           <td>{planet.gravity}</td>
+           <td>{planet.terrain}</td>
+           <td>{planet.surface_water}</td>
+           <td>{planet.population}</td>
+           <td>{planet.films}</td>
+           <td>{planet.created}</td>
+           <td>{planet.edited}</td>
+           <td>{planet.url}</td>
+         </tr>
+       ))}
+    </tbody>
+  );
+}
+export default TableBody;
