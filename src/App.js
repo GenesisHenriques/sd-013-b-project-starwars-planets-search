9efb13d71f4070faee9c4131588f61403fa5fa9f
Vini <viniderp@gmail.com>
2021-09-17 20:22:58 -0300

    req 2 final

--- src/App.js
+++ src/App.js
@@ -1,108 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
-// import PlanetsContext from './contexts/PlanetsContext';
-// import Search from './components/Search';
+// import PlanetsContext from './context/PlanetsContext';
 
-import testData from './testData';
+import PlanetsProvider from './context/PlanetsProvider';
+import Table from './components/Table';
+import Filters from './components/Filters';
 
-const INITIAL_STATE = {
-  data: testData,
-};
+// import testData from './testData';
+
+// const INITIAL_STATE = {
+//   data: testData,
+// };
 
 function App() {
   // const planets = useContext(PlanetsContext) || { state: { data: testData } };
-  const [text, setText] = useState('');
-  const [state, setState] = useState(INITIAL_STATE);
+  // const planets = useContext(PlanetsContext);
+  // const [text, setText] = useState('');
+  // const [state, setState] = useState(INITIAL_STATE);
 
-  // console.log(state);
-  useEffect(() => {
-    global.fetch('https://swapi-trybe.herokuapp.com/api/planets/?page=1')
-      .then((res) => res.json())
-      .then((resData) => {
-        console.log(resData);
-        setState((s) => ({ ...s, data: resData }));
-      });
-  }, []);
+  // // console.log(state);
+  // useEffect(() => {
+  //   global.fetch('https://swapi-trybe.herokuapp.com/api/planets/?page=1')
+  //     .then((res) => res.json())
+  //     .then((resData) => {
+  //       console.log(resData);
+  //       setState((s) => ({ ...s, data: resData }));
+  //     });
+  // }, []);
 
-  const planets = { state, setState };
-  const { count, results } = planets.state.data;
+  // // const planets = { state, setState };
+  // const { count, results } = planets.state.data;
 
-  ///
+  // ///
 
-  let filteredPlanets;
+  // let filteredPlanets;
 
-  if (planets.state.filters) {
-    const { name } = planets.state.filters.filterByName;
+  // if (planets.state.filters) {
+  //   const { name } = planets.state.filters.filterByName;
 
-    filteredPlanets = results.filter((el) => el.name.includes(name));
-  }
+  //   filteredPlanets = results.filter((el) => el.name.includes(name));
+  // }
 
-  const changeHandler = (e) => {
-    setText(e.target.value);
-    planets.setState({ ...planets.state,
-      filters: {
-        filterByName: {
-          name: e.target.value,
-        },
-      },
-    });
-    // planets.addFilter(e.target.value);
-  };
+  // const changeHandler = (e) => {
+  //   setText(e.target.value);
+  //   planets.setState({ ...planets.state,
+  //     filters: {
+  //       filterByName: {
+  //         name: e.target.value,
+  //       },
+  //     },
+  //   });
+  //   // planets.addFilter(e.target.value);
+  // };
 
   // console.log(planets);
 
-  fetch('');
+  // fetch('');
   return (
-    <>
-      {/* <Search /> */}
-      <input
-        data-testid="name-filter"
-        onChange={ changeHandler }
-        type="text"
-        value={ text }
-      />
-      <table style={ { width: '100%' } }>
-        <thead>
-          <tr>
-            <th>name</th>
-            <th>rotation_period</th>
-            <th>orbital_period</th>
-            <th>diameter</th>
-            <th>climate</th>
-            <th>gravity</th>
-            <th>terrain</th>
-            <th>surface_water</th>
-            <th>population</th>
-            <th>films</th>
-            <th>created</th>
-            <th>el.edited</th>
-            <th>el.url</th>
-          </tr>
-        </thead>
-        <tbody>
-          { count > 0 && (
-            (filteredPlanets || planets.state.data.results).map((el, i) => (
-              <tr key={ i }>
-                <td>{el.name}</td>
-                <td>{el.rotation_period}</td>
-                <td>{el.orbital_period}</td>
-                <td>{el.diameter}</td>
-                <td>{el.climate}</td>
-                <td>{el.gravity}</td>
-                <td>{el.terrain}</td>
-                <td>{el.surface_water}</td>
-                <td>{el.population}</td>
-                <td>{el.films}</td>
-                <td>{el.created}</td>
-                <td>{el.edited}</td>
-                <td>{el.url}</td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-    </>
+    <PlanetsProvider>
+      <Filters />
+      <Table />
+    </PlanetsProvider>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -2,43 +2,29 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import PlanetsContext from './PlanetsContext';
-import testData from '../testData';
-
-const INITIAL_STATE = {
-  data: testData,
-};
-
-// export const PlanetsContext = createContext(
-//   { state: INITIAL_STATE,
-//     addFilter: () => console.log(1),
-//   },
-// );
+import fetchPlanets from '../services/api';
 
 const PlanetsProvider = (props) => {
   const { children } = props;
-  const [state, setState] = useState(INITIAL_STATE);
+  const [data, setData] = useState({});
+  const [filters, setFilters] = useState();
 
-  // console.log(state);
   useEffect(() => {
-    global.fetch('https://swapi-trybe.herokuapp.com/api/planets/?page=1')
-      .then((res) => res.json())
-      .then((resData) => {
-        console.log(resData);
-        setState((s) => ({ ...s, data: resData }));
-      });
+    fetchPlanets().then((resData) => setData(resData));
   }, []);
 
-  const store = { state, setState };
-
   return (
-    <PlanetsContext.Provider value={ store }>
+    <PlanetsContext.Provider value={ { data, filters, setFilters } }>
       { children }
     </PlanetsContext.Provider>
   );
 };
 
 PlanetsProvider.propTypes = {
-  children: PropTypes.objectOf(PropTypes.symbol).isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default PlanetsProvider;
