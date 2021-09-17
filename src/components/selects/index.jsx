2b4e4c00dddcc4ca60fb607e35412f05e4d42c8c
Joao Victor <joaogmavictor@gmail.com>
2021-09-16 21:07:01 -0300

    Concluindo 4

--- src/components/selects/index.jsx
+++ src/components/selects/index.jsx
@@ -1,31 +1,36 @@
-import React, { useCallback, useState, useContext } from 'react';
+import React, { useCallback, useState, useContext, useEffect } from 'react';
 import Column from './components/Column';
 import Comparison from './components/Comparison';
 import Value from './components/Value';
 import MainContext from '../../context/MainContext';
 
-const optionsSelect = [
-  'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
-];
-
 export default function Selects() {
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
   const [value, setValue] = useState('0');
+  const [optionsSelect, setOptionsSelect] = useState([
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ]);
 
   const { hadlerFilterByComparison } = useContext(MainContext);
 
   const verification = useCallback(() => {
     const index = optionsSelect.indexOf(column);
     optionsSelect.splice(index, 1);
-  }, [column]);
+  }, [column, optionsSelect]);
 
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
     const obj = { column, comparison, value };
-    verification(obj);
     hadlerFilterByComparison(obj);
+    verification();
   }, [column, comparison, hadlerFilterByComparison, value, verification]);
+
+  useEffect(() => {
+    setOptionsSelect([
+      'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+    ]);
+  }, []);
 
   return (
     <form
