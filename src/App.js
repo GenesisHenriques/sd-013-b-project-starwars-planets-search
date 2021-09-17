f75a1a268e6e7f557cb6c684d8931dcaf07051c9
pedrodumorro <pa.coelho.castro@gmnail.com>
2021-09-17 17:18:07 -0300

    Req 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SearchForm from './components/SearcForm';
 import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
   return (
     <PlanetsProvider>
+      <SearchForm />
       <Table />
     </PlanetsProvider>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,28 +3,49 @@ import PlanetContext from '../context/PlanetsContext';
 import TableHeader from './TableHeader';
 
 function Table() {
-  const { data } = useContext(PlanetContext);
+  const { data, filterMethod, filters } = useContext(PlanetContext);
+
+  const NAME = 'name';
+
+  let planetsToRender = [];
+
+  function getName() {
+    const { filterByName: { name } } = filters;
+    return name;
+  }
+
+  switch (filterMethod) {
+  case NAME:
+    planetsToRender = data.filter(
+      (planet) => (planet.name.toLowerCase().includes(getName().toLowerCase())),
+    );
+    break;
+  default:
+    planetsToRender = data;
+    break;
+  }
+
   return (
     <table>
       <TableHeader />
       {
-        data.map((planetInfo, id) => (
+        planetsToRender.map((planetInfo, id) => (
           <tr
             key={ id }
           >
-            <td>{ planetInfo.climate }</td>
-            <td>{ planetInfo.created }</td>
-            <td>{ planetInfo.diameter }</td>
-            <td>{ planetInfo.edited }</td>
-            <td>{ planetInfo.films }</td>
-            <td>{ planetInfo.gravity }</td>
-            <td>{ planetInfo.name }</td>
-            <td>{ planetInfo.orbital_period }</td>
-            <td>{ planetInfo.population }</td>
-            <td>{ planetInfo.rotation_period }</td>
-            <td>{ planetInfo.surface_water }</td>
-            <td>{ planetInfo.terrain }</td>
-            <td>{ planetInfo.url }</td>
+            <td>{planetInfo.climate}</td>
+            <td>{planetInfo.created}</td>
+            <td>{planetInfo.diameter}</td>
+            <td>{planetInfo.edited}</td>
+            <td>{planetInfo.films}</td>
+            <td>{planetInfo.gravity}</td>
+            <td>{planetInfo.name}</td>
+            <td>{planetInfo.orbital_period}</td>
+            <td>{planetInfo.population}</td>
+            <td>{planetInfo.rotation_period}</td>
+            <td>{planetInfo.surface_water}</td>
+            <td>{planetInfo.terrain}</td>
+            <td>{planetInfo.url}</td>
           </tr>
         ))
       }
