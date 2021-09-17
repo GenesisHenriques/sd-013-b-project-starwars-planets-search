0a4305cfa1b4b924e27f953d61b9defa20b5557f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-16 23:04:36 -0300

    implementa a logica dos requisitor 2,3,4 e 5

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
+const INITIAL_STATE = {
+  filterByName: {
+    name: '',
+  },
+  filterByNumericValues: [],
+  order: {},
+};
+
 function MyProvider({ children }) {
   const [planetsData, setPlanetsData] = useState([]);
+  const [filters, setFilters] = useState(INITIAL_STATE);
+  const [planetsFiltered, setPlanetsFiltered] = useState([]);
+  const [filterOptions, setFilterOptions] = useState({ column: 'population',
+    comparison: 'maior que',
+    value: '' });
 
+  const { filterByName: { name }, filterByNumericValues } = filters;
   useEffect(() => {
     async function requiredData() {
       const END_POINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
@@ -20,8 +34,32 @@ function MyProvider({ children }) {
     requiredData();
   }, []);
 
+  useEffect(() => {
+    let result = [...planetsData];
+    result = result.filter(({ name: planet }) => planet.toLowerCase()
+      .includes(name.toLowerCase()));
+    filterByNumericValues.forEach(({ column, comparison, value }) => {
+      result = result.filter((planet) => {
+        if (comparison === 'maior que') {
+          return Number(planet[column]) > Number(value);
+        }
+        if (comparison === 'menor que') {
+          return Number(planet[column]) < Number(value);
+        }
+        return Number(planet[column]) === Number(value);
+      });
+    });
+    setPlanetsFiltered(result);
+  }, [filterByNumericValues, name, planetsData]);
+
   const contextValue = {
     planetsData,
+    planetsFiltered,
+    filters,
+    setFilters,
+    setFilterOptions,
+    filterOptions,
+    setPlanetsFiltered,
   };
 
   return (
