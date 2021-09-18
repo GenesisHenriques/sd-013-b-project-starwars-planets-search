891753c5b267399a8c582d58a3ebab0d9b50dfda
Jessica Naiara <jessicanaiarag@gmail.com>
2021-09-18 19:12:42 -0300

    requisito 3 e 4

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,61 @@
-import React, { useContext, useState } from 'react';
+// Código elaborado de acordo com a aula 18.3 da turma 12 da Trybe.
+// Código elaborado de acordo com exemplos do site https://www.w3schools.com/tags.
+// Código elaborado de acordo com exemplos do site https://devmedia.
+
+import React, { useContext, useState, useEffect } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
   const { data } = useContext(StarWarsContext);
-  const [filters, setFilters] = useState({
-    filterByName: {
-      name: '',
-    },
+  const [filteredData, setFilteredData] = useState([]);
+
+  const [filterByName, setFilterByName] = useState('');
+  const [arrayNumericFilters, setArrayNumericFilters] = useState([]);
+  const [newFilter, setNewfilter] = useState({
+    column: '',
+    comparison: '',
+    value: '',
   });
+
+  const columns = ['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const [filteredColumns, setFilteredColumns] = useState([...columns]);
+
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
+
+  useEffect(() => {
+    console.log(filteredColumns);
+  }, [filteredColumns]);
+
+  const applyNumberFilter = (planet) => {
+    switch (newFilter.comparison) {
+    case 'maior que':
+      return (Number(planet[newFilter.column]) > Number(newFilter.value));
+    case 'menor que':
+      return (Number(planet[newFilter.column]) < Number(newFilter.value));
+    case 'igual a':
+      return (Number(planet[newFilter.column]) === Number(newFilter.value));
+    default:
+      return false;
+    }
+  };
+
+  const handleAddFilter = () => {
+    const contem = arrayNumericFilters.find(
+      (filter) => filter.column === newFilter.column,
+    );
+    if (!contem) {
+      setArrayNumericFilters([...arrayNumericFilters, newFilter]);
+      const newData = filteredData.filter((planet) => applyNumberFilter(planet));
+      setFilteredData(newData);
+      console.log(newFilter);
+      const newColumns = filteredColumns.filter((filter) => filter !== newFilter.column);
+      setFilteredColumns(newColumns);
+      console.log(newColumns);
+    }
+  };
 
   return (
     <div>
@@ -18,14 +66,69 @@ function Table() {
             data-testid="name-filter"
             type="text"
             name="filterName"
-            value={ filters.filterByName.name }
-            onChange={ (event) => setFilters({
-              filterByName: {
-                name: event.target.value,
-              },
+            value={ filterByName }
+            onChange={ (event) => {
+              const { target: { value } } = event;
+              setFilterByName(value);
+              const newData = data.filter((planet) => planet.name.includes(value));
+              setFilteredData(newData);
+            } }
+          />
+        </label>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="column-filter">
+          <select
+            name="column-filter"
+            data-testid="column-filter"
+            value={ newFilter.column }
+            onChange={ (event) => setNewfilter({
+              ...newFilter,
+              column: event.target.value,
+            }) }
+          >
+            { filteredColumns.map((column) => (
+              <option key={ column } value={ column }>{column}</option>
+            )) }
+          </select>
+        </label>
+        <label htmlFor="comparison-filter">
+          <select
+            name="comparison-filter"
+            data-testid="comparison-filter"
+            value={ newFilter.comparison }
+            onChange={ (event) => setNewfilter({
+              ...newFilter,
+              comparison: event.target.value,
+            }) }
+          >
+            <option value=""> </option>
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="value-filter">
+          <input
+            type="number"
+            name="value-filter"
+            data-testid="value-filter"
+            value={ newFilter.value }
+            onChange={ (event) => setNewfilter({
+              ...newFilter,
+              value: event.target.value,
             }) }
           />
         </label>
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleAddFilter }
+        >
+          Adicionar
+
+        </button>
       </div>
       <br />
       <table border="1">
@@ -49,8 +152,7 @@ function Table() {
         <tbody>
           {data === undefined
             ? <tr>Loading...</tr>
-            : data
-              .filter((planet) => planet.name.includes(filters.filterByName.name))
+            : filteredData
               .map((planet, index) => (
                 <tr key={ index }>
                   <td>{planet.name}</td>
