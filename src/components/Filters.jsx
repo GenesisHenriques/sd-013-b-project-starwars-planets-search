11ce1a0c02d5041d980ddd643db1cf3c2cd43f6a
Rafael Reis <srp.rafael@gmail.com>
2021-09-19 20:26:08 -0300

    configurando inputs requisito 3

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -16,6 +16,30 @@ function Filters() {
           onChange={ (ev) => handleNameFilter(ev.target.value) }
         />
       </label>
+      <div>
+        <label htmlFor="category">
+          Escolha a categoria:
+          <select name="category" id="category" data-testid="column-filter">
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+        </label>
+        <label htmlFor="rule">
+          Escolha a regra de comparação:
+          <select name="rule" id="rule" data-testid="comparison-filter">
+            <option value="maior-que">maior que</option>
+            <option value="menor-que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="value" data-testid="value-filter">
+          <input type="number" name="value" id="value" />
+        </label>
+        <button type="submit" data-testid="button-filter">Filtrar</button>
+      </div>
     </div>
   );
 }
