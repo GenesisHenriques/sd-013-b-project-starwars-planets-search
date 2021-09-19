cb359f46fb396a6767c935afc91ba0456ca6564b
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-09-19 12:22:37 -0300

    realiza requisito 6 e refatora ordenaçao da tabela

--- src/hooks/useOrderPlanets.js
+++ src/hooks/useOrderPlanets.js
@@ -11,13 +11,12 @@ function useOrderPlanets() {
 
   const orderPlanets = useCallback((newData) => {
     // extraido de: https://pt.stackoverflow.com/questions/46600/como-ordenar-uma-array-de-objetos-com-array-sort
-    const num = -1;
+    // faz com que o array seja ordenado numericamente e de ordem crescente ou alfabética caso se strings
     switch (sort) {
     case 'ASC':
-      return newData.sort((a, b) => ((a[column] < b[column]) ? num : 1));
-      // return newData.sort((a, b) => a[column] - b[column]);
+      return newData.sort((a, b) => -(a[column] < b[column]) || a[column] - b[column]);
     case 'DESC':
-      return newData.sort((b, a) => (a[column] - b[column]));
+      return newData.sort((b, a) => (a[column] - b[column]) || -(a[column] < b[column]));
     default:
       break;
     }
